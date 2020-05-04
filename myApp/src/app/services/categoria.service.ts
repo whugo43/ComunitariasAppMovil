deb14d1e8d8d1f8fd609d5036ed983adaf464244
hugo wong <whugo43@hotmail.com>
2020-05-03 23:16:49 -0500

    servicio categoria

---
+++ myApp/src/app/services/categoria.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
+
+import { HttpClientModule } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CategoriaService {
+
+  constructor(private http:HttpClient) {
+   console.log("categorias de donaciones");
+   }
+   getCategorias(){
+     return this.http.get('');
+   }
+}

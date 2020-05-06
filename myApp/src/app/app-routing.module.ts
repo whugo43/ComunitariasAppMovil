ff7420bf0219f6cf0d05396a5149eda523c93e20
leonel de la a <dalede@espol.edu.ec>
2020-05-05 20:24:53 -0600

    correcion al merge

--- myApp/src/app/app-routing.module.ts
+++ myApp/src/app/app-routing.module.ts
@@ -1,52 +1,3 @@
-<<<<<<< HEAD
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [
-  {
-    path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  },
-  {
-    path: 'donaciones',
-    loadChildren: () => import('./donaciones/donaciones.module').then( m => m.DonacionesPageModule)
-  },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: 'about',
-    loadChildren: () => import('./about/about.module').then( m => m.AboutPageModule)
-  },
- 
-  {
-    path: 'contacts',
-    loadChildren: () => import('./contacts/contacts.module').then( m => m.ContactsPageModule)
-  },
-  {
-    path: 'empresas',
-    loadChildren: () => import('./empresas/empresas.module').then( m => m.EmpresasPageModule)
-  },
-  {
-    path: 'categoria',
-    loadChildren: () => import('./categoria/categoria.module').then( m => m.CategoriaPageModule)
-  },
-  {
-    path: 'centro-acopio',
-    loadChildren: () => import('./centro-acopio/centro-acopio.module').then( m => m.CentroAcopioPageModule)
-  },
-  {
-    path: 'generar-categoria',
-    loadChildren: () => import('./categoria/generar-categoria/generar-categoria.module').then( m => m.GenerarCategoriaPageModule)
-  },
-  {
-    path: 'generar-donacion',
-    loadChildren: () => import('./donaciones/generar-donacion/generar-donacion.module').then( m => m.GenerarDonacionPageModule)
-  },  {
-    path: 'distribucion',
-    loadChildren: () => import('./distribucion/distribucion.module').then( m => m.DistribucionPageModule)
-=======
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
@@ -97,11 +48,13 @@ const routes: Routes = [
     path: 'generar-categoria',
     loadChildren: () => import('./categoria/generar-categoria/generar-categoria.module').then( m => m.GenerarCategoriaPageModule)
   },
-  
   {
     path: 'generar-donacion',
     loadChildren: () => import('./donaciones/generar-donacion/generar-donacion.module').then( m => m.GenerarDonacionPageModule)
->>>>>>> 3444ac9eb7468dbc9935af38e2b2dd56be2838c5
+  },
+  {
+    path: 'distribucion',
+    loadChildren: () => import('./distribucion/distribucion.module').then( m => m.DistribucionPageModule)
   }
 
 ];

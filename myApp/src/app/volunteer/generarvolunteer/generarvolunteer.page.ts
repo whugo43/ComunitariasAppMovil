c4a8119cc023a62cbd1012bc132e3c0ba1a256d6
@whugo43 <whugo43@hotmail.com>
2020-06-24 21:27:00 -0500

    plantilla generar voluntario

--- myApp/src/app/volunteer/generarvolunteer/generarvolunteer.page.ts
+++ myApp/src/app/volunteer/generarvolunteer/generarvolunteer.page.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AlertController } from '@ionic/angular';
+import { VoluntariosService } from 'src/app/services/voluntarios/voluntarios.service';
+import { ActivityService } from 'src/app/services/activity/activity.service';
 @Component({
   selector: 'app-generarvolunteer',
   templateUrl: './generarvolunteer.page.html',
   styleUrls: ['./generarvolunteer.page.scss'],
 })
 export class GenerarvolunteerPage implements OnInit {
+  activities;
+  formularios={
+    activities:[], 
+    firstName: '',
+    lastName: '',
+    social:'',
+    schedule:'',
+    phoneNumber:''
+    }
 
-  constructor() { }
+  constructor(public alertController: AlertController,
+              public voluntariosService: VoluntariosService,
+              public activityService: ActivityService) { }
 
   ngOnInit() {
+    this.activityService.getActivity()
+    .subscribe(
+    (data)=>{this.activities=data},
+    (error)=>{console.log(error)}
+    );
+  }
+
+  postvolunteer(){
+    
   }
 
 }

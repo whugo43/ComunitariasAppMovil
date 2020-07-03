d5587acd8accbe006cb5232fd96f0514e5b6b942
AnaBelen93 <anita-belen93@hotmail.com>
2020-07-02 23:17:04 -0500

    corregido el conflicto

--- myApp/src/app/login/resetpwd/resetpwd.page.ts
+++ myApp/src/app/login/resetpwd/resetpwd.page.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Router} from '@angular/router';
 import { AlertController } from '@ionic/angular';
-import { FormBuilder, Validators} from '@angular/forms';
-
+import { FormBuilder, FormGroup, Validators} from '@angular/forms';
 @Component({
   selector: 'app-resetpwd',
   templateUrl: './resetpwd.page.html',
@@ -16,14 +15,6 @@ export class ResetpwdPage implements OnInit {
 
   ngOnInit() {
   }
-  public resetpwdForm;
-  emailChanged: boolean = false;
-  submitAttempt: boolean = false;
-  loading: any;
-  let EMAIL_REGEXP = /^[a-z0-9!#$%&'*+\/=?^_`{|}~.-]+@[a-z0-9]([a-z0-9-]*[a-z0-9])?(\.[a-z0-9]([a-z0-9-]*[a-z0-9])?)*$/i;
-    this.resetpwdForm = formBuilder.group({
-      email: ['', Validators.compose([Validators.required, Validators.pattern(EMAIL_REGEXP)])]
-    });
-  }
+ 
 
 }
